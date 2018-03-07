06a5145a5f8e776d6b7da05f9ae08e9bd1e47df0
Ehsan Totoni <ehsan.totoni@intel.com>
2018-03-07 12:05:52 -0500

    convert all kitti labels

--- convert_kitti_labels.py
+++ convert_kitti_labels.py
@@ -3,9 +3,8 @@ import numpy as np
 import h5py
 import glob
 
-
-fname = "/home/etotoni/pse-hpc/python/datasets/KITTI/training/label_2/006655.txt"
-dtype_dict = {'type': str, 'truncated': np.float32,
+def convert_labels():
+    dtype_dict = {'type': str, 'truncated': np.float32,
                'occluded': np.uint8, 'alpha': np.float32,
                'bbox_l': np.float32, 'bbox_t': np.float32,
                'bbox_r': np.float32, 'bbox_b': np.float32,
@@ -13,22 +12,33 @@ dtype_dict = {'type': str, 'truncated': np.float32,
                'dim_l': np.float32, 'loc_x': np.float32,
                'loc_y': np.float32, 'loc_z': np.float32,
                'rot_y': np.float32}
+    label_dir = "/home/etotoni/pse-hpc/python/datasets/KITTI/training/label_2/*.txt"
+    files = sorted(glob.glob(label_dir))
+    n_files = len(files)
+    all_data = pd.DataFrame()
+    for i in range(n_files):
+        fname = files[i]
+        df = pd.read_csv(fname, sep=' ', names=list(dtype_dict.keys()),
+                         dtype=dtype_dict)
+        type_convert = {'Car': 0, 'Van': 1, 'Truck': 2,
+                             'Pedestrian': 3, 'Person_sitting': 4, 'Cyclist': 5, 'Tram': 6,
+                             'Misc': 7, 'DontCare': 8}
+
+        # convert string type
+        df['type'] = df['type'].apply(lambda a: type_convert[a])
+        df['img_id'] = np.full(len(df), i, dtype=np.uint32)
+        all_data = all_data.append(df, ignore_index=True)
+
+    dtype_dict['type'] = np.uint8
+    dtype_dict['img_id'] = np.uint32
+    N = len(all_data)
+    f = h5py.File("kitt_labels.h5", "w")
+    for name, dt in dtype_dict.items():
+        dset = f.create_dataset(name, (N,), dtype=dt)
+        dset[:] = all_data[name]
+
+    f.close()
 
 
-df = pd.read_csv(fname, sep=' ', names=list(dtype_dict.keys()),
-                 dtype=dtype_dict)
-type_convert = {'Car': 0, 'Van': 1, 'Truck': 2,
-                     'Pedestrian': 3, 'Person_sitting': 4, 'Cyclist': 5, 'Tram': 6,
-                     'Misc': 7, 'DontCare': 8}
-
-df['type'] = df['type'].apply(lambda a: type_convert[a])
-dtype_dict['type'] = np.uint8
-
-N = len(df)
-f = h5py.File("kitt_labels.h5", "w")
-for name, dt in dtype_dict.items():
-    dset = f.create_dataset(name, (N,), dtype=dt)
-    dset[:] = df[name]
-
-f.close()
-print(df)
+if __name__ == '__main__':
+    convert_labels()
